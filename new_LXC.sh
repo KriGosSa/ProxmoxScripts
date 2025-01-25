09989eda8a59197fc4b952c240be9a6c11b19855
KriGosSa <KriGosSa@users.noreply.github.com>
2025-01-25 19:06:31 +0100

    correct concat in container

--- new_LXC.sh
+++ new_LXC.sh
@@ -323,12 +323,12 @@ export LOGIN_UNAME=$LOGIN_UNAME
 
 
 IN_CONTAINER=$(envsubst < "$SCRIPT_DIR/colors_format_icons.sh")
-IN_CONTAINER.=$(envsubst < "$SCRIPT_DIR/error_handler.sh")
-IN_CONTAINER.=$(envsubst < "$SCRIPT_DIR/message_spinner.sh")
-IN_CONTAINER.=$(envsubst < "$SCRIPT_DIR/setup_in_new_container.sh")
+IN_CONTAINER="($IN_CONTAINER)$(envsubst < "$SCRIPT_DIR/error_handler.sh")"
+IN_CONTAINER="($IN_CONTAINER)$(envsubst < "$SCRIPT_DIR/message_spinner.sh")"
+IN_CONTAINER="($IN_CONTAINER)$(envsubst < "$SCRIPT_DIR/setup_in_new_container.sh")"
 
 #cat "$IN_CONTAINER"
-#echo "end debug"
+echo "end debug"
 
 # lxc-attach -n "$CONTAINER_ID" -- bash -c "$(cat)" param1 "$CONTAINER_ID"
 lxc-attach -n "$CONTAINER_ID" -- bash -c "$IN_CONTAINER" param1 "$CONTAINER_ID"
